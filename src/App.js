60f4d6cecb54d33a8812078527655850a6128f3c
Autímio Meira Brito Filho <autimio.dev@gmail.com>
2020-01-28 23:23:05 -0300

    Application styling

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import React from 'react';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
+import './Main.css';
 
 function App() {
+  
+
   return (
     <div id="app">
       <aside>
@@ -36,7 +39,52 @@ function App() {
         </form>
       </aside>
       <main>
-        {/* devs */}
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/19158164?s=460" />
+              <div className="user-info">
+                <strong>Autímio Meira Brito Filho</strong>
+                <span>ReactJS, NodeJS, R, Java</span>
+              </div>
+            </header>
+            <p>Bachelor of Computer Science from the Pontifical Catholic University of Goiás.</p>
+            <a href="https://github.com/autimio">Acessar peprfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/19158164?s=460" />
+              <div className="user-info">
+                <strong>Autímio Meira Brito Filho</strong>
+                <span>ReactJS, NodeJS, R, Java</span>
+              </div>
+            </header>
+            <p>Bachelor of Computer Science from the Pontifical Catholic University of Goiás.</p>
+            <a href="https://github.com/autimio">Acessar peprfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/19158164?s=460" />
+              <div className="user-info">
+                <strong>Autímio Meira Brito Filho</strong>
+                <span>ReactJS, NodeJS, R, Java</span>
+              </div>
+            </header>
+            <p>Bachelor of Computer Science from the Pontifical Catholic University of Goiás.</p>
+            <a href="https://github.com/autimio">Acessar peprfil no Github</a>
+          </li>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars0.githubusercontent.com/u/19158164?s=460" />
+              <div className="user-info">
+                <strong>Autímio Meira Brito Filho</strong>
+                <span>ReactJS, NodeJS, R, Java</span>
+              </div>
+            </header>
+            <p>Bachelor of Computer Science from the Pontifical Catholic University of Goiás.</p>
+            <a href="https://github.com/autimio">Acessar peprfil no Github</a>
+          </li>
+        </ul>
       </main>
     </div>
   );
