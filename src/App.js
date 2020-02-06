8e1c3be7cdfaf495a347743047b5ec972ff81ecb
Autímio Meira Brito Filho <autimio.dev@gmail.com>
2020-02-05 23:19:21 -0300

    consuming api data and componentization

--- src/App.js
+++ src/App.js
@@ -1,89 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import api from './services/api';
 
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
+import DevItem from './components/DevItem'
+import DevForm from './components/DevForm'
+
+
 function App() {
-  
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+      setDevs(response.data);
+    }
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post('/devs', data);
+    setDevs([...devs, response.data]);
+  }
+
 
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" required />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" required />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" required />
-            </div>
-
-            <button type="submit">Salvar</button>
-          </div>
-        </form>
+       <DevForm onSubmit={handleAddDev}/>
       </aside>
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/19158164?s=460" />
-              <div className="user-info">
-                <strong>Autímio Meira Brito Filho</strong>
-                <span>ReactJS, NodeJS, R, Java</span>
-              </div>
-            </header>
-            <p>Bachelor of Computer Science from the Pontifical Catholic University of Goiás.</p>
-            <a href="https://github.com/autimio">Acessar peprfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/19158164?s=460" />
-              <div className="user-info">
-                <strong>Autímio Meira Brito Filho</strong>
-                <span>ReactJS, NodeJS, R, Java</span>
-              </div>
-            </header>
-            <p>Bachelor of Computer Science from the Pontifical Catholic University of Goiás.</p>
-            <a href="https://github.com/autimio">Acessar peprfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/19158164?s=460" />
-              <div className="user-info">
-                <strong>Autímio Meira Brito Filho</strong>
-                <span>ReactJS, NodeJS, R, Java</span>
-              </div>
-            </header>
-            <p>Bachelor of Computer Science from the Pontifical Catholic University of Goiás.</p>
-            <a href="https://github.com/autimio">Acessar peprfil no Github</a>
-          </li>
-          <li className="dev-item">
-            <header>
-              <img src="https://avatars0.githubusercontent.com/u/19158164?s=460" />
-              <div className="user-info">
-                <strong>Autímio Meira Brito Filho</strong>
-                <span>ReactJS, NodeJS, R, Java</span>
-              </div>
-            </header>
-            <p>Bachelor of Computer Science from the Pontifical Catholic University of Goiás.</p>
-            <a href="https://github.com/autimio">Acessar peprfil no Github</a>
-          </li>
+            {devs.map(dev => (
+             <DevItem key={dev._id} dev={dev}/>
+            ))}
         </ul>
       </main>
     </div>
